69abc16d59c33858eb3f521f56513f97a74c4845
Robert Brotherus <robert@iki.fi>
2024-02-25 21:36:12 +0200

    Years-table with points-summary

--- src/sports/years.cljs
+++ src/sports/years.cljs
@@ -1,12 +1,26 @@
 (ns sports.years
-  (:require [re-frame.core :as rf]))
+  (:require [clojure.string :as s]
+            [cljs.pprint :refer [pprint]]
+            [re-frame.core :as rf]
+            [sports.calculations :as calc]
+            [sports.year :as year-view]))
 
 (defn view []
-  (let [years @(rf/subscribe [:years])]
-    [:div
-     (for [year (sort (keys years))]
-       (let [on-click #(rf/dispatch [:show-year year])]
-         ^{:key year} [:div [:button.navigation {:on-click on-click} year]]))]))
+  (let [player-cols (s/join " " (repeat (count calc/players) "80px"))
+        years @(rf/subscribe [:years])]
+    [:div.grid {:style {:grid-template-columns (str "100px auto " player-cols " 1fr")}}
+     [:div.bold "Year"] [:div.bold "Sessions"]
+     (into [:<>] (for [p calc/players] [:div.bold p])) [:div]
+     [:div.row-line]
+     (into [:<>]
+           (for [year (sort (keys years))]
+             (let [on-click #(rf/dispatch [:show-year year])
+                   {:keys [sessions players]} @(rf/subscribe [:year-summary year])]
+               [:<>
+                [:div.link {:on-click on-click} year]
+                [:div (count sessions)]
+                [year-view/player-points-cells players true?]
+                [:div]])))]))
 
 (rf/reg-event-db :show-year
   (fn [db [_ year]]
